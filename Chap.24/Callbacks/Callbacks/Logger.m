5ed67cd95983ae27173c2d481e500a265dadeb83
심연진 <yeonjin@prodigycom.co.kr>
2016-02-22 15:48:33 +0900

    Chap.24 콜백 - 도우미 객체

--- Chap.24/Callbacks/Callbacks/Logger.m
+++ Chap.24/Callbacks/Callbacks/Logger.m
@@ -9,8 +9,48 @@
 #import "Logger.h"
 
 @implementation Logger
+
 - (void)sayOuch:(NSTimer *)t
 {
     NSLog(@"Ouch!");
 }
+
+// 데이터가 도착할 때마다 호출된다.
+- (void)connection:(NSURLConnection *)connection
+    didReceiveData:(NSData *)data
+{
+    NSLog(@"received %lu bytes", [data length]);
+    
+    //변경할 수 있는 데이터를 만든다(존재하지 않을 때).
+    if (!incomingData) {
+        incomingData = [[NSMutableData alloc] init];
+    }
+    [incomingData appendData:data];
+}
+
+//마지막 데이터가 처리될 때 호출된다.
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"Got it all");
+    
+    NSString *string = [[NSString alloc] initWithData:incomingData
+                                             encoding:NSUTF8StringEncoding];
+    
+    incomingData = nil;
+    NSLog(@"string has %lu characters", [string length]);
+    
+    //가져온 파일 전체를 확인하려면 다음 행에서 주석을 제거한다.
+    //NSLog(@"The whole string is %@", string);
+}
+
+// 가져오기가 실패하면 호출된다.
+- (void)connection:(NSURLConnection *)connection
+didFailWithError:(NSError *)error
+{
+    NSLog(@"connection failed: %@", [error localizedDescription]);
+    incomingData = nil;
+}
+
+
+
 @end
