be03850be36656a290f2b5646c5a1a461f576557
심연진 <yeonjin@prodigycom.co.kr>
2016-02-22 14:32:54 +0900

    Chap.24 콜백 - 타깃과 액션

---
+++ Chap.24/Callbacks/Callbacks/Logger.h
@@ -0,0 +1,13 @@
+//
+//  Logger.h
+//  Callbacks
+//
+//  Created by 심연진 on 2016. 2. 22..
+//  Copyright © 2016년 BigNerdRanch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logger : NSObject
+- (void)sayOuch:(NSTimer *)t;
+@end
---
+++ Chap.24/Callbacks/Callbacks/Logger.m
@@ -0,0 +1,16 @@
+//
+//  Logger.m
+//  Callbacks
+//
+//  Created by 심연진 on 2016. 2. 22..
+//  Copyright © 2016년 BigNerdRanch. All rights reserved.
+//
+
+#import "Logger.h"
+
+@implementation Logger
+- (void)sayOuch:(NSTimer *)t
+{
+    NSLog(@"Ouch!");
+}
+@end
