2398adef8db78db81238d5f73e5727e2dd047386
심연진 <yeonjin@prodigycom.co.kr>
2016-02-22 14:06:26 +0900

    Revert "@"/Users/coriel/Desktop/google.png""
    This reverts commit c2fae6ca2257b888c70977c4d93992a73c4df43a.

--- Chap.23/Stringz/Stringz/main.m
+++ Chap.23/Stringz/Stringz/main.m
@@ -35,11 +35,6 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"Success!");
         
-        NSData *readData = [NSData dataWithContentsOfFile:@"/Users/coriel/Desktop/google.png"];
-        NSLog(@"The file read from the disk has %lu bytes", [readData length]);
-                            
-                            
-        
     }
     return 0;
 }
